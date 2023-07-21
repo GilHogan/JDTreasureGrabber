dfb95aeed90af82aa1413399346253fc51376fd7
GilHogan <33539824+GilHogan@users.noreply.github.com>
2023-07-21 13:46:42 +0800

    first commit

---
+++ server/api.js
@@ -0,0 +1,16 @@
+/**
+ * 会用到的京东接口地址
+ * */
+const API = {
+	api_jd_hostname: "api.m.jd.com",
+	api_jd_path: "/api",
+	api_jd: "https://api.m.jd.com/api",
+	item_url: "https://paipai.jd.com/auction-detail/",
+	login_url: "https://passport.jd.com/new/login.aspx?from=paipai_pc&ReturnUrl=https%3A%2F%2Fpaipai.jd.com%2Fauction-list",
+	login_success_redirect_url: "https://paipai.jd.com/auction-list",
+	offer_price_function_id: "paipai.auction.offerPrice",
+	web_api_header_referer: "https://paipai.jd.com/",
+	app_api_header_referer: "https://paipai.m.jd.com/",
+	image_url: "https://img10.360buyimg.com/"
+};
+module.exports = API;
