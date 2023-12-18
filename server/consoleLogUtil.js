ea251a851d73a7c8f9b2f74885ffc2bbe3612c17
hogan <g.guhuangjin@gmail.com>
2023-12-18 10:04:50 +0800

    logUtil尝试抓取异常

--- server/consoleLogUtil.js
+++ server/consoleLogUtil.js
@@ -1,35 +1,38 @@
 const { BrowserWindow } = require('electron');
 
-function log (...msg) {
-    const windows = BrowserWindow.getAllWindows();
-    if (!windows) {
-        return;
-    }
-    let currentWindow;
-    for (let i = 0; i < windows.length; i++) {
-        const win = windows[i];
-        console.log("windows = ", win.title)
-        if (win.title == "京东夺宝岛助手") {
-            currentWindow = win;
-            break;
+function log(...msg) {
+    try {
+        const windows = BrowserWindow.getAllWindows();
+        if (!windows) {
+            return;
         }
-    }
-    if (currentWindow) {
-        // 获取参数
-        const params = msg.map((arg) => {
-            if (typeof arg === "string") {
-                return arg;
-            } else {
-                return JSON.stringify(arg);
+        let currentWindow;
+        for (let i = 0; i < windows.length; i++) {
+            const win = windows[i];
+            if (win && win.title == "京东夺宝岛助手") {
+                currentWindow = win;
+                break;
             }
-        });
+        }
+        if (currentWindow) {
+            // 获取参数
+            const params = msg.map((arg) => {
+                if (typeof arg === "string") {
+                    return arg;
+                } else {
+                    return JSON.stringify(arg);
+                }
+            });
 
-        const str = params.join(" ");
-        currentWindow.webContents && currentWindow.webContents.send("fromMain", { event: "console", data: str });
+            const str = params.join(" ");
+            currentWindow.webContents && currentWindow.webContents.send("fromMain", { event: "console", data: str });
+        }
+    } catch (error) {
+        console.error("consoleLogUtil log error:", error);
     }
 }
 
-function error (...msg) {
+function error(...msg) {
     log(msg);
 }
 module.exports = { log, error };
